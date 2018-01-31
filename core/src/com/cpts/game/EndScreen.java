815a786f266fb39be3979b4aed661032f1bdb3b2
Kristopher <kristopher.tefft@gmail.com>
2018-01-31 14:04:07 -0800

    menu screen mostly implemented

---
+++ core/src/com/cpts/game/EndScreen.java
@@ -0,0 +1,56 @@
+package com.cpts.game;
+
+import com.badlogic.gdx.Screen;
+
+public class EndScreen implements Screen{
+
+	private MainGameClass parent;
+	 
+	public EndScreen(MainGameClass mainGameClass){
+		parent = mainGameClass;     
+	}
+	
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- core/src/com/cpts/game/MainGameClass.java
+++ core/src/com/cpts/game/MainGameClass.java
@@ -1,6 +1,7 @@
 package com.cpts.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,53 +10,45 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.Input.Keys;
 
-public class MainGameClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture cat;
-	Texture img;
-	Rectangle box;
+public class MainGameClass extends Game {
+	
+	private LoadingScreen loadingScreen;
+	private PreferencesScreen preferencesScreen;
+	private MenuScreen menuScreen;
+	private MainScreen mainScreen;
+	private EndScreen endScreen;
+	 
+	public final static int MENU = 0;
+	public final static int PREFERENCES = 1;
+	public final static int APPLICATION = 2;
+	public final static int ENDGAME = 3;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("konosuba-2-aqua-drinking.png");
-		cat = new Texture("cat.jpg");
-		
-		//draw a box
-		box = new Rectangle();
-	    box.x = 800 / 2 - 64 / 2;
-	    box.y = 20; 
-	    box.width = 64;
-	    box.height = 64;
-		
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(cat, box.x, box.y);
-		batch.end();
-		
-	      // process user input
-	      if(Gdx.input.isTouched()) {
-	         Vector3 touchPos = new Vector3();
-	         touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-	         box.x = touchPos.x - 64 / 2;
-	      }
-	      if(Gdx.input.isKeyPressed(Keys.LEFT)) box.x -= 200 * Gdx.graphics.getDeltaTime();
-	      if(Gdx.input.isKeyPressed(Keys.RIGHT)) box.x += 200 * Gdx.graphics.getDeltaTime();
-	      
-	      // make sure the bucket stays within the screen bounds
-	      if(box.x < 0) box.x = 0;
-	      if(box.x > 1038 - 64) box.x = 1038 - 64;
+		loadingScreen = new LoadingScreen(this);
+		setScreen(loadingScreen);
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void changeScreen(int screen){
+		switch(screen){
+			case MENU:
+				if(menuScreen == null) menuScreen = new MenuScreen(this);
+	                        this.setScreen(menuScreen);
+				break;
+			case PREFERENCES:
+				if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this);
+				this.setScreen(preferencesScreen);
+				break;
+			case APPLICATION:
+				if(mainScreen == null) mainScreen = new MainScreen(this);
+				this.setScreen(mainScreen);
+				break;
+			case ENDGAME:
+				if(endScreen == null) endScreen = new EndScreen(this);
+				this.setScreen(endScreen);
+				break;
+		}
 	}
+	
+
 }
