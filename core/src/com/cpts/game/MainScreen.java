44b10bd587ca739f693289da067ea9f0f2322266
Patrick <patrick@wless-user-172019166201.temp.wsu.edu>
2018-02-10 14:07:36 -0800

    player class

--- core/src/com/cpts/game/MainScreen.java
+++ core/src/com/cpts/game/MainScreen.java
@@ -16,11 +16,13 @@ public class MainScreen implements Screen {
 	Texture cat;
 	Texture img;
 	Rectangle box;
+	Player player;
 
 	 
 	public MainScreen(MainGameClass mainGameClass){
 		parent = mainGameClass;
 		batch = new SpriteBatch();
+		player = new Player();
 		img = new Texture("konosuba-2-aqua-drinking.png");
 		cat = new Texture("cat.jpg");
 		
@@ -43,11 +45,13 @@ public class MainScreen implements Screen {
 	
 	@Override
 	public void render(float delta) {
+		player.handleMovement();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.draw(cat, box.x, box.y);
+		batch.draw(player.sprite, player.sprite.getX(), player.sprite.getY());
 		batch.end();
 		
 	      // process user input
